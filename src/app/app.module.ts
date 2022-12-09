72416983df37d980a5e91a635f819beb74f7f35f
Akefa <akefatarannum@gmail.com>
2022-12-09 18:28:14 +0530

    I have completed My assesmnet for front end develper role

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DashboredComponent } from './dashbored/dashbored.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboredComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashbored/dashbored.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-dashbored',
+  templateUrl: './dashbored.component.html',
+  styleUrls: ['./dashbored.component.css']
+})
+export class DashboredComponent implements OnInit {
+  POSTS: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 7;
+  tableSizes: any = [3, 6, 9, 12];
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  fetchPosts(): void {
+    this.postService.getAllPosts().subscribe(
+      (response) => {
+        this.POSTS = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.fetchPosts();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.fetchPosts();
+  }
+
+}
